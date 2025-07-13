7dc7f569b3a665b639d80b1a44836a61aeb85ab1
rushikesh9592 <126757669+rushikesh9592@users.noreply.github.com>
2025-07-13 15:22:23 +0530

    Added Port in Program.cs

--- Program.cs
+++ Program.cs
@@ -49,6 +49,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
