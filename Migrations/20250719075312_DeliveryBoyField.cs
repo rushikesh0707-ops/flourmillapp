5bdd08f6e1698c3de1f04d88fd0c99231fa9b57d
rushikesh9592 <126757669+rushikesh9592@users.noreply.github.com>
2025-07-19 13:24:04 +0530

    Delivery Boy API email field

---
+++ Migrations/20250719075312_DeliveryBoyField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlourmillAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeliveryBoyField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "DeliveryBoys",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "DeliveryBoys");
+        }
+    }
+}
